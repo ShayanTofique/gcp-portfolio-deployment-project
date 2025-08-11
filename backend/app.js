8aec8c79d784ac799b3ac92d6b7af12cf42f06fa
ShayanTofique <shaikhshayan137@gmail.com>
2025-08-11 09:46:58 +0000

    change backend app.js to serve public folder

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/projects', projectRoutes);
 
 
