e13f6c2236f184939811265795957b1f0a443022
ShayanTofique <shaikhshayan137@gmail.com>
2025-08-15 15:36:05 +0000

    modified app.js to allow access

--- backend/app.js
+++ backend/app.js
@@ -36,4 +36,5 @@ app.get(/^\/(?!.*\.\w+$)/, (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
