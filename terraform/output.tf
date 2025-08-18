bae0e4f91444a7c59eb35dd6e10549bdc5b77a4d
ShayanTofique <shaikhshayan137@gmail.com>
2025-08-18 15:19:10 +0000

    added code to get output of mongodbURI

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,9 @@
-output "vm_ip" {
-  description = "Public IP of the VM"
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+# output "vm_ip" {
+#   description = "Public IP of the VM"
+#   value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+# }
+
+output "mongodb_uri" {
+  value     = var.mongodb_uri
+  sensitive = true
 }
