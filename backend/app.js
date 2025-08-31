acf3629acff756619d7c7d66d942a985ba27e2a4
ShayanTofique <shaikhshayan137@gmail.com>
2025-08-31 06:07:08 +0000

    Add Prometheus metrics collection to Node.js app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const projectRoutes = require('./routes/project');
 
+// Prometheus metrics
+const client = require('prom-client');
+
 require('dotenv').config();
 
 // DB connection
@@ -10,6 +13,39 @@ const connectMongo = require('./config/mongo');
 connectMongo();
 
 const app = express();
+
+// Create Prometheus metrics
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+const httpRequestTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code']
+});
+
+// Middleware to collect metrics
+app.use((req, res, next) => {
+  const start = Date.now();
+  
+  res.on('finish', () => {
+    const duration = (Date.now() - start) / 1000;
+    const route = req.route ? req.route.path : req.path;
+    
+    httpRequestDuration
+      .labels(req.method, route, res.statusCode)
+      .observe(duration);
+    
+    httpRequestTotal
+      .labels(req.method, route, res.statusCode)
+      .inc();
+  });
+  
+  next();
+});
 
 // Middleware
 app.use(bodyParser.json());
@@ -21,6 +57,14 @@ app.use('/api/projects', projectRoutes);
 app.use((req, res, next) => {
   console.log(`➡️ ${req.method} ${req.url}`);
   next();
+});
+
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  client.register.metrics().then(metrics => {
+    res.end(metrics);
+  });
 });
 
 // Routes
